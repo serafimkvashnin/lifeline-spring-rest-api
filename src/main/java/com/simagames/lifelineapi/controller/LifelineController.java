78c023159708675fd528318e8024f83c2e507fed
Serafim Kvashnin <serafim274715@gmail.com>
2024-06-25 00:14:55 +0300

    Close API with OAuth using Google authentication provider

--- src/main/java/com/simagames/lifelineapi/controller/LifelineController.java
+++ src/main/java/com/simagames/lifelineapi/controller/LifelineController.java
@@ -1,5 +1,6 @@
-package com.simagames.lifelineapi;
+package com.simagames.lifelineapi.controller;
 
+import com.simagames.lifelineapi.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
