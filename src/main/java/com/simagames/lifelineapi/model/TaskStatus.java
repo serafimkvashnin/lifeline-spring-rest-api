785db21bf623ccef3e4d394c540e0db4a1759db1
Serafim Kvashnin <serafim274715@gmail.com>
2024-06-25 11:42:29 +0300

    Remove Task field recursion from JSON

--- src/main/java/com/simagames/lifelineapi/model/TaskStatus.java
+++ src/main/java/com/simagames/lifelineapi/model/TaskStatus.java
@@ -1,5 +1,6 @@
 package com.simagames.lifelineapi.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,6 +21,7 @@ public class TaskStatus {
     private String comment;
 
     @ManyToOne
+    @JsonIgnore
     @JoinColumn(name = "task_id")
     private Task task;
 }
